044d319f245c0293f0fcf3b5b38dcdb3b3d5b5b5
Boardmate <yourgmail@gmail.com>
2025-12-04 13:22:00 +0530

    Update settings

--- onlinequiz/settings.py
+++ onlinequiz/settings.py
@@ -147,3 +147,4 @@ EMAIL_HOST_PASSWORD = 'waig ntos tnqc zkyy' # host email password required
 # otherwise you will get SMTPAuthenticationError at /contactus
 # this process is required because google blocks apps authentication by default
 EMAIL_RECEIVING_USER = ['contactboardmate@gmail.com'] # email on which you will receive messages sent from website
+STATIC_ROOT = BASE_DIR / 'staticfiles'
